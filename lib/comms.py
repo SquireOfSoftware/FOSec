693c55042ab8115eb4f7f7d12503f2405e4239d1
Joseph Tran <11204688@student.uts.edu.au>
2016-09-11 21:03:40 +1000

    further tinkering

--- lib/comms.py
+++ lib/comms.py
@@ -6,8 +6,6 @@ from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA256
 from Crypto import Random  
 from lib.crypto_utils import ANSI_X923_pad, ANSI_X923_unpad
-from lib.helpers import read_hex
-
 
 from dh import create_dh_key, calculate_dh_secret
 
@@ -19,12 +17,10 @@ class StealthConn(object):
         self.server = server
         self.verbose = verbose
         self.initiate_session()
-        self.shared_hash = None
 
     def initiate_session(self):
         # Perform the initial connection handshake for agreeing on a shared secret
 
-        ### TODO: Your code here!
         # This can be broken into code run just on the server or just on the client
         if self.server or self.client:
             my_public_key, my_private_key = create_dh_key()
@@ -37,18 +33,20 @@ class StealthConn(object):
 			#Shared key is in hash format                           
             self.shared_hash = codecs.decode(self.shared_hash, 'hex_codec')
 
-        IV = Random.get_random_bytes((AES.block_size)
-		
+        IV = Random.get_random_bytes((AES.block_size));
+        print("IV: ", IV, len(IV));
         #would you use a random number and wrap round to 56 bits?
-        self.cipher = AES.new(self.shared_hash[:32], AES.MODE_CBC, IV)
-        #print("length of hash: ", len(shared_hash));
+        self.cipher = AES.new(self.shared_hash[:32], AES.MODE_CBC, IV);
+        print("length of hash: ", len(self.shared_hash));
         #print("type of hash: ", type(shared_hash));
         #self.HMAC is using half of the shared key as the MAC(unique ID) and Hashed it
-        self.HMAC = HMAC.new(self.shared_hash[32:].encode("ascii"))
+        self.HMAC = HMAC.new(self.shared_hash[32:], digestmod = SHA256)
 		
     def send(self, data):
         if self.cipher:
-            #print("Sending data now.");
+            IV = Random.get_random_bytes(AES.block_size);
+            #self.cipher = AES.new(self.shared_hash[:32], AES.MODE_CBC, IV);
+
             # data must be padded
 			#Hashed the message as part of the HMAC
             hmac = self.HMAC;
@@ -56,16 +54,19 @@ class StealthConn(object):
 
             #data = data[:-data[-1]]
 			#HMAC has been appended to the message to ensure integrity
+            #message = IV + data + hmac.digest();
+            IV = self.cipher.IV;
             message = IV + data + hmac.digest();
-            #print("message: ", message);
-            #print("message length: ", len(message));
-            
-            # pad message here
-            pad_length = BLOCK_SIZE - (len(message) % BLOCK_SIZE);
-            message += bytes([pad_length])*pad_length;
-
+            #message = IV + bytes(hmac.hexdigest() + data.decode("ascii"), "ascii");
             print("message: ", message);
             print("message length: ", len(message));
+            
+            # pad message here
+            pad_length = AES.block_size - (len(message) % AES.block_size);
+            message += bytes([pad_length])*(pad_length);
+            #message = ANSI_X923_pad(message, AES.block_size);
+            print("padded message: ", message);
+            #print("pad length: ", pad_length);
 
             #message = message[:-message[-1]];
 
@@ -76,7 +77,7 @@ class StealthConn(object):
                 print("Sending packet of length {}".format(len(encrypted_data)))
         else:
             encrypted_data = data
-
+        print("sending cipher: ", self.cipher);
         # Encode the data's length into an unsigned two byte int ('H')
         pkt_len = struct.pack('H', len(encrypted_data))
         self.conn.sendall(pkt_len)
@@ -89,20 +90,24 @@ class StealthConn(object):
         print("packet len packed: ", pkt_len_packed);
         unpacked_contents = struct.unpack('H', pkt_len_packed)
         #pkt_len = unpacked_contents[len(unpacked_contents) - 1];
+        #print("length of hash: ", len(self.shared_hash));
         pkt_len = unpacked_contents[0];
         print("unpacked_contents: ", unpacked_contents);
         print("pkt_len: ", pkt_len);
         # last bit contains the length of the original message
         # need to split this out also take out the 32 bit hmac
         print("unpacking now");
-
         encrypted_data = self.conn.recv(pkt_len)
+        IV = encrypted_data[:AES.block_size];
+        #self.cipher = AES.new(self.shared_hash[:32], AES.MODE_CBC, IV);
         # this bit is correct
         print("encrypted_data: ", encrypted_data);
         print("self.cipher: ", self.cipher);
         if self.cipher:
             print("decrypting now");
-            data = self.cipher.decrypt(encrypted_data);
+            IV = encrypted_data[:AES.block_size];
+            #print("iv comparison: {} {} ", IV, self.cipher.iv)
+            data = self.cipher.decrypt(encrypted_data[AES.block_size:]);
             print("decrypted data: ", data);
             if self.verbose:
                 print("Receiving packet of length {}".format(pkt_len))
@@ -110,14 +115,28 @@ class StealthConn(object):
                 print("Original data: {}".format(data))
         else:
             data = encrypted_data;
-        
-        print("data: ", str(data[BLOCK_SIZE:]));
+        IV = data[:AES.block_size];
+        #data = ANSI_X923_unpad(data, AES.block_size);
+        print("IV, ", IV);
+        print("data: ", data[16:]);
+        #data = data[16:];
         #removing padding
 
+        pad_length = data[-1];
+        print("pad_length: ", pad_length);
+        print("original message: ", data[AES.block_size:len(data) - pad_length]);
+        data_with_hmac = data[AES.block_size: len(data) - pad_length];
+        print("data_with_hmac: ", data_with_hmac);
         #split the HMAC from 
-
+        hmac = data_with_hmac[len(data_with_hmac) - AES.block_size:];
+        print("hmac: ", hmac);
+        cmd = data_with_hmac[:-AES.block_size];
+        print("cmd: ", cmd, len(cmd), type(cmd));
+        #data = data_with_hmac[:len(data_with_hmac) - AES.block_size];
+        #print("data: ", data_with_hmac[:len(data_with_hmac) - AES.block_size], len(data_with_hmac));
         # this needs to be the command for the packet
-        return data
-
+        #return data[AES.block_size:-pad_length];
+        #return cmd;
+        return data;
     def close(self):
         self.conn.close()
